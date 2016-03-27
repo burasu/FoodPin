d9bdab7a33756bb7fd23ce70b032bcf258c53538
Blas Fernandez <burasu@gmail.com>
2016-03-27 21:40:13 +0200

    Añadiendo que el usuario pueda guardar un restaurante

--- AddRestaurantController.swift
+++ AddRestaurantController.swift
@@ -12,6 +12,14 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
 
     @IBOutlet var imageView:UIImageView!
     
+    @IBOutlet var nameTextField:UITextField!
+    @IBOutlet var typeTextField:UITextField!
+    @IBOutlet var locationTextField:UITextField!
+    @IBOutlet var yesButton:UIButton!
+    @IBOutlet var noButton:UIButton!
+    
+    var isVisited = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -49,7 +57,55 @@ class AddRestaurantController: UITableViewController, UIImagePickerControllerDel
         imageView.contentMode = UIViewContentMode.ScaleAspectFill
         imageView.clipsToBounds = true
         
+        let leadingConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Leading, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Leading, multiplier: 1, constant: 0)
+        leadingConstraint.active = true
+        
+        let trailingConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Trailing, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Trailing, multiplier: 1, constant: 0)
+        trailingConstraint.active = true
+        
+        let topConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Top, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Top, multiplier: 1, constant: 0)
+        topConstraint.active = true
+        
+        let bottomConstraint = NSLayoutConstraint(item: imageView, attribute: NSLayoutAttribute.Bottom, relatedBy: NSLayoutRelation.Equal, toItem: imageView.superview, attribute: NSLayoutAttribute.Bottom, multiplier: 1, constant: 0)
+        bottomConstraint.active = true
+        
         dismissViewControllerAnimated(true, completion: nil)
     }
     
+    @IBAction func save(sender:UIBarButtonItem) {
+        let name = nameTextField.text
+        let type = typeTextField.text
+        let location = locationTextField.text
+        
+        // Validate input fields
+        if name == "" || type == "" || location == "" {
+            let alertController = UIAlertController(title: "Oops", message: "We can't proceed because one of the fields is blank. Please note that all fields are required.", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler: nil))
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        // Print input data to console
+        print("Name: \(name)")
+        print("Type: \(type)")
+        print("Location: \(location)")
+        print("Have you been here: \(isVisited)")
+        
+        // Dismiss the view controller
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func toggleBeenHereButton(sender: UIButton) {
+        // Yes button clicked
+        if sender == yesButton {
+            isVisited = true
+            yesButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+            noButton.backgroundColor = UIColor.grayColor()
+        } else if sender == noButton {
+            isVisited = false
+            yesButton.backgroundColor = UIColor.grayColor()
+            noButton.backgroundColor = UIColor(red: 235.0/255.0, green: 73.0/255.0, blue: 27.0/255.0, alpha: 1.0)
+        }
+    }
 }
