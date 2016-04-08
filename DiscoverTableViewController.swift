ab75f4346928f19145d50f209bdf2316ca92166e
Blas Fernandez <burasu@gmail.com>
2016-04-08 23:19:08 +0200

    Implementación de refeshControl

--- DiscoverTableViewController.swift
+++ DiscoverTableViewController.swift
@@ -12,8 +12,11 @@ import CloudKit
 class DiscoverTableViewController: UITableViewController {
 
     @IBOutlet var spinner:UIActivityIndicatorView!
+    @IBOutlet var table: UITableView!
     
     var restaurants:[CKRecord] = []
+    
+    var imageCache:NSCache = NSCache()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,15 @@ class DiscoverTableViewController: UITableViewController {
         spinner.startAnimating()
         
         getRecordsFromCloud()
+        
+        // Control para el Pull Refresh
+        refreshControl = UIRefreshControl()
+        refreshControl?.backgroundColor = UIColor.whiteColor()
+        refreshControl?.tintColor = UIColor.grayColor()
+        refreshControl?.addTarget(self, action: #selector(DiscoverTableViewController.getRecordsFromCloud), forControlEvents: UIControlEvents.ValueChanged)
+        
+        table.addSubview(refreshControl!)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -68,6 +80,7 @@ class DiscoverTableViewController: UITableViewController {
             }
             
             print("Successfully retrieve the data from iCloud")
+            self.refreshControl?.endRefreshing()
             NSOperationQueue.mainQueue().addOperationWithBlock() {
                 self.spinner.stopAnimating()
                 self.tableView.reloadData()
@@ -100,30 +113,43 @@ class DiscoverTableViewController: UITableViewController {
         // Establecemos una imagen por defecto
         cell.imageView?.image = UIImage(named: "photoalbum")
         
-        // Obtenemos las imagenes de la nube en segundo plano
-        let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
-        let fetchRecordsImageOperation = CKFetchRecordsOperation(recordIDs: [restaurant.recordID])
-        fetchRecordsImageOperation.desiredKeys = ["image"]
-        fetchRecordsImageOperation.queuePriority = .VeryHigh
+        // Comprobamos si la imagen está almacenada en la cache
+        if let imageFileURL = imageCache.objectForKey(restaurant.recordID) as? NSURL {
         
-        fetchRecordsImageOperation.perRecordCompletionBlock = {(record:CKRecord?, recordID:CKRecordID?, error:NSError?) -> Void in
-            if (error != nil) {
-                print("Failed to get restaurant image: \(error!.localizedDescription)")
-                return
-            }
+            // Obtenemos la imagen de la caché
+            print("Get image from cache")
+            cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageFileURL)!)
             
-            if let restaurantRecord = record {
-                NSOperationQueue.mainQueue().addOperationWithBlock() {
-                    if let imageAsset = restaurantRecord.objectForKey("image") as? CKAsset {
-                        cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageAsset.fileURL)!)
-                        
-                        print("Mostrada imagen: \(imageAsset.fileURL)")                    
+        } else {
+        
+            // Obtenemos las imagenes de la nube en segundo plano
+            let publicDatabase = CKContainer.defaultContainer().publicCloudDatabase
+            let fetchRecordsImageOperation = CKFetchRecordsOperation(recordIDs: [restaurant.recordID])
+            fetchRecordsImageOperation.desiredKeys = ["image"]
+            fetchRecordsImageOperation.queuePriority = .VeryHigh
+        
+            fetchRecordsImageOperation.perRecordCompletionBlock = {(record:CKRecord?, recordID:CKRecordID?, error:NSError?) -> Void in
+                if (error != nil) {
+                    print("Failed to get restaurant image: \(error!.localizedDescription)")
+                    return
+                }
+            
+                if let restaurantRecord = record {
+                    NSOperationQueue.mainQueue().addOperationWithBlock() {
+                        if let imageAsset = restaurantRecord.objectForKey("image") as? CKAsset {
+                            cell.imageView?.image = UIImage(data: NSData(contentsOfURL: imageAsset.fileURL)!)
+                            
+                            // Añadimos la imagen a la caché para cargarla más rápido
+                            self.imageCache.setObject(imageAsset.fileURL, forKey: restaurant.recordID)
+                            
+                            print("Mostrada imagen: \(imageAsset.fileURL)")
+                        }
                     }
                 }
             }
-        }
         
-        publicDatabase.addOperation(fetchRecordsImageOperation)
+            publicDatabase.addOperation(fetchRecordsImageOperation)
+        }
 
         return cell
     }
